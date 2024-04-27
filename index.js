dc7813d63287db5b06ba13eb440d2dce4f5f6495
shajid444 <mdshajidhasan76@gmail.com>
2024-04-27 21:50:06 +0600

    after completing the delete operation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -45,6 +45,17 @@ async function run() {
         const result = await placeCollection.insertOne(newPlace);
         res.send(result);
     })
+
+    app.delete('/place/:id', async(req, res)=>{
+
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await placeCollection.deleteOne(query);
+        res.send(result);
+
+    })
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
